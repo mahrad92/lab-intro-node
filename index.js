d678a013ff9ba49bb88ceb6984953e982bd981e6
mask <mahsaaaaa@gmail.com>
2021-11-09 14:07:52 +0100

    lab-intro-node-solution/mahsa

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items;
+    this.length = items.length;
+  }
+  add(itemA) {
+    this.items.push(itemA);
+    this.items = this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos > this.items.length || pos < 0) throw new Error("OutOfBounds");
+    else return this.items[pos];
+  }
 
-  add(item) {}
+  max() {
+    let m;
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let m = this.items.reduce((a, c) => (a > c ? a : c));
+      return m;
+    }
+  }
 
-  get(pos) {}
+  min() {
+    let m;
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let m = this.items.reduce((a, c) => (a < c ? a : c));
+      return m;
+    }
+  }
 
-  max() {}
+  sum() {
+    let m = this.items.reduce((a, c) => a + c, 0);
+    return m;
+  }
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    let m = this.items.reduce((a, c) => a + c, 0) / this.items.length;
+    return m;
+  }
 }
+let a = new SortedList([1, 2, 3]);
+console.log("m", a.avg());
 
 module.exports = SortedList;
